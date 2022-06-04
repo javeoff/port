42a1ca1edc7f66e99d78b74384d7b78259b8c11d
Daniil Jave <javeoff@yandex.ru>
2022-06-04 15:35:54 +0300

    feat: add eslint configuration for the monorepo

---
+++ packages/eslint/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    ['arrow-body-style']: ['error', 'as-needed'],
+    ['prettier/prettier']: 'error',
+    ['import/no-extraneous-dependencies']: 'error',
+    ['import/no-unresolved']: 'off',
+    ['import/no-useless-path-segments']: 'error',
+    ['import/order']: [
+      'error',
+      {
+        ['alphabetize']: {
+          caseInsensitive: true,
+          order: 'asc',
+        },
+        ['groups']: [
+          ['builtin', 'external', 'object', 'type'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+        ['newlines-between']: 'always',
+      },
+    ],
+    ['react/require-default-props']: 'off',
+    ['react/jsx-props-no-spreading']: 'off',
+    ['react/no-array-index-key']: 'off',
+    ['react/prop-types']: 'off',
+    ['react/react-in-jsx-scope']: 'off',
+  },
+  ignorePatterns: ['src/**/*.test.ts'],
+};
---
+++ packages/ui/src/components/Button/Button.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'react';
+
+export const Button: FC = () => <div>Test Text</div>;
